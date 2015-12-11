3aa6da6830f47e316812d209919e6b57b1d68a27
Citizen01 <jeanbaptiste.e@hotmail.fr>
2015-12-11 03:16:23 +0100

    Added ban function + Inverted if to reduce nesting

--- business/players.lua
+++ business/players.lua
@@ -55,26 +55,47 @@ end
 
 -- @API("POST", "/players/kick")
 function kick( form, user )
-	if form.id then
-		local id = tonumber(form.id)
-		if id then
-			local player = getApiElementByID(id)
-			if player then
-				local reason = form.reason or ""
-				outputServerLog("[API] "..tostring(getPlayerName(player)).." has been kicked by "..tostring(getAccountName(user))..".")
-				local bool = kickPlayer(player, reason)
-				if bool then
-					return 200, nil, ""
-				else
-					return 500, "An error occured !", nil
-				end
-			else
-				return 404, "Player not found !", nil
-			end
-		else
-			return 400, "Number expected for id parameter !", nil
-		end
+	if not form.id then return 400, "Missing id parameter !", nil end
+	
+	local id = tonumber(form.id)
+	if not id then return 400, "Number expected for id parameter !", nil end
+	
+	local player = getApiElementByID(id)
+	if not player or getElementType(player) ~= "player" then return 404, "Player not found !", nil end
+	
+	local ip = form.ip and form.ip == "true"
+	local reason = form.reason or ""
+	
+	if kickPlayer(player, reason) then
+		outputServerLog("[API] "..tostring(getPlayerName(player)).." has been kicked by "..tostring(getAccountName(user))..".")
+		return 200, nil, ""
 	else
-		return 400, "Missing id parameter !", nil
+		return 500, "An error occured !", nil
+	end
+end
+
+-- @API("POST", "/players/ban")
+function ban( form, user )
+	if not form.id then return 400, "Missing id parameter !", nil end
+	
+	local id = tonumber(form.id)
+	if not id then return 400, "Number expected for id parameter !", nil end
+	
+	local player = getApiElementByID(id)
+	if not player or getElementType(player) ~= "player" then return 404, "Player not found !", nil end
+	
+	local isIp = form.ip and form.ip == "true"
+	local isUsername = form.username and form.username == "true"
+	local isSerial = form.serial and form.serial == "true" or false
+	local responsiblePlayer = user or nil
+	local reason = form.reason or ""
+	local secs = secs and tonumber(form.secs) or 0
+	
+	if banPlayer(player, isIp, isUsername, isSerial, responsiblePlayer, reason, secs) then
+		local time = secs == 0 and "permanently" or tostring(secs).." secs"
+		outputServerLog("[API] "..tostring(getPlayerName(player)).." has been banned by "..tostring(getAccountName(user)).." ("..time..").")
+		return 200, nil, ""
+	else
+		return 500, "An error occured !", nil
 	end
 end
