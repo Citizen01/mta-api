c61b24d5d0c2ccdf9112277774c7fc08804e036b
Citizen01 <jeanbaptiste.e@hotmail.fr>
2016-05-07 02:23:42 +0200

    Added /vehicles/update/{id}

--- business/vehicles.lua
+++ business/vehicles.lua
@@ -24,6 +24,23 @@ function getVehicleEntity( veh )
 	}
 end
 
+function updateVehicleEntity( veh, json )
+	if json.alpha ~= nil then setElementAlpha(veh, json.alpha) end
+	if json.dimension ~= nil then setElementDimension(veh, json.dimension) end
+	if json.health ~= nil then setElementHealth(veh, json.health) end
+	if json.interior ~= nil then setElementInterior(veh, json.interior) end
+	if json.isDamageProof ~= nil then setVehicleDamageProof(veh, json.isDamageProof) end
+	if json.isLocked ~= nil then setVehicleLocked(veh, json.isLocked) end
+	if json.isFuelTankExplodable ~= nil then setVehicleFuelTankExplodable(veh, json.isFuelTankExplodable) end
+	if json.model ~= nil then setElementModel(veh, json.model) end
+	if json.paintjob ~= nil then setVehiclePaintjob(veh, json.paintjob) end
+	if json.plateText ~= nil then setVehiclePlateText(veh, json.plateText) end
+	if json.position ~= nil then setElementPosition(veh, json.position.x, json.position.y, json.position.z) end
+	if json.rotation ~= nil then setElementRotation(veh, json.rotation.x, json.rotation.y, json.rotation.z) end
+
+	return true
+end
+
 ----------------------------------------------
 
 -- @API("GET", "/vehicles/{id}")
@@ -50,4 +67,18 @@ function getVehicles( form, user )
 
 	if #_vehs == 0 then return 200, "[]" end
 	return 200, _vehs
+end
+
+-- @API("PUT", "/vehicles/update/{id}")
+function updateVehicle( form, user )
+	local json = json.decode(form.json)
+	-- outputConsole(tostring(var_dump("-v", json)))
+
+	local id = tonumber(form.id)
+	if not id then return 400, nil, "Number expected for id parameter !" end
+
+	local vehicle = getApiElementByID(id)
+	if not vehicle or getElementType(vehicle) ~= "vehicle" then return 404, nil, "Vehicle not found !" end
+
+	return updateVehicleEntity(vehicle, json)
 end
