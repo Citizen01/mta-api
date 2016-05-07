1aed1321661f4310d27881b936a88516cbdea3a0
Citizen01 <jeanbaptiste.e@hotmail.fr>
2016-05-07 02:27:53 +0200

    Fixed bug where getting the player json wouldn't work if he was inside a vehicle (typo)

--- business/players.lua
+++ business/players.lua
@@ -22,7 +22,7 @@ function getPlayerEntity( player )
 		serial = getPlayerSerial(player),
 		skin = getElementModel(player),
 		team = (function () local t = getPlayerTeam(player); return t and getTeamName(t) end)(),
-		vehicle = (function () local v = getPedOccupiedVehicle(player); return v and getApiElementId(v) end)(),
+		vehicle = (function () local v = getPedOccupiedVehicle(player); return v and getApiElementID(v) end)(),
 		version = getPlayerVersion(player),
 		wantedLevel = getPlayerWantedLevel(player),
 		weapon = getPedWeapon(player),
